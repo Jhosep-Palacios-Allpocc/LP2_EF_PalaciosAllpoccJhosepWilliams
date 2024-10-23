5b09229fdf581e7bc061b43126bb8ccd4a2cc374
Jhosep-Palacios-Allpocc <i202314569@cibertec.edu.pe>
2024-10-23 15:44:46 -0500

    Estructura Inicial del Examen Final

---
+++ src/main/java/pe/com/cibertec/LP2_EF_PalaciosJhosep/Lp2EfPalaciosJhosepApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.LP2_EF_PalaciosJhosep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2EfPalaciosJhosepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2EfPalaciosJhosepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/LP2_EF_PalaciosJhosep/Lp2EfPalaciosJhosepApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.LP2_EF_PalaciosJhosep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2EfPalaciosJhosepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
